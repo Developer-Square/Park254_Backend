0bb787d6f403f5a5b7b9f5450465be9fd3fd29b9
saisilinus <saisilinus19@gmail.com>
2021-04-08 05:27:11 -0400

    feat: add landing page

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ const { authLimiter } = require('./middlewares/rateLimiter');
 const routes = require('./routes/v1');
 const { errorConverter, errorHandler } = require('./middlewares/error');
 const ApiError = require('./utils/ApiError');
+const path = require('path');
 
 const app = express();
 
@@ -52,6 +53,9 @@ if (config.env === 'production') {
 
 // v1 api routes
 app.use('/v1', routes);
+
+// static routes
+app.use(express.static(path.join(__dirname, 'public')));
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
