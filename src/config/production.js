58a1c49240192bfb102e0e1e841b2a0814c34276
linus saisi <42277372+saisilinus@users.noreply.github.com>
2021-04-16 10:09:31 +0300

    refactor: mongodb atlas url

--- src/config/production.js
+++ src/config/production.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
   JWT_SECRET: process.env.PRODUCTION_JWT_SECRET,
   SMTP_PASSWORD: process.env.PRODUCTION_SMTP_PASSWORD,
-  MONGODB_ATLAS_URL: `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@parking-app-server.sgvnp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  MONGODB_ATLAS_URL: `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@parking-app-server.sgvnp.mongodb.net/Parking?retryWrites=true&w=majority`,
 };
