4378f01baffbccfa4ce9bf408b531b629d901fd7
saisilinus <saisilinus19@gmail.com>
2021-05-17 03:57:35 -0400

    removed shortcode

--- src/config/mpesa.config.js
+++ src/config/mpesa.config.js
@@ -5,7 +5,6 @@ const options = {
   consumer_secret: 'GKoQCvLMpkwrlgTh',
   passKey: 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919',
   BusinessShortCode: 174379,
-  ShortCode: 174379,
   SecurityCredential:
     'V0a35DDV6XOguhnADH69VY4UBbt+RJ9igh/Jqed4QRzXqKV0GZ6waof3y8nBXB1mRw4xSCSrh2NC+TwGrxTUR0X1rwI52a/hmNSMAVy/5ms0u8pKN4ER6YJTmWsRQSmvj1kyVRjV5DWGIDPQ4sgvnAHFvlEwD+WMNvNDTFWJMQLhbPtcwBhTVmxoxTpu0DpiwW6+hKpMOD45rMWft1T7g+im01/nkZT9BYwUgLmRuUN+qrLNMWvs2KViaHGwYzoOa0pA9YIKvOryDIsaj6qU5ufxbfQcAuTdqYIJLN+N5VxQlROsJ9HIXMy9mBdhNZYu0cT0r+zPPCmkgsOGbhUgGg==',
   Initiator: 'GKoQCvLMpkwrlgTh',
