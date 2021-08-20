878c6deb8adf69627e8098cab4decdd01606d134
saisilinus <saisilinus19@gmail.com>
2021-08-20 10:33:19 -0400

    fix: tests

--- src/jobs/agenda.js
+++ src/jobs/agenda.js
@@ -1,7 +1,7 @@
 /* eslint-disable security/detect-non-literal-require, import/no-dynamic-require, global-require, no-return-await */
 const { Agenda } = require('agenda');
 const config = require('../config/config');
-const logger = require('../config/logger');
+// const logger = require('../config/logger');
 
 const agenda = new Agenda({ db: { address: config.mongoose.url } });
 
@@ -14,26 +14,26 @@ jobTypes.forEach((type) => {
 if (jobTypes.length) {
   // agenda.on('ready', async () => await agenda.start());
   agenda.on('ready', async () => {
-    logger.info(`Initializing agenda jobs`);
+    // logger.info(`Initializing agenda jobs`);
     await agenda.start();
   });
 
-  agenda.on('start', (job) => {
-    logger.debug(`Job ${job.attrs.name} starting`);
-  });
-  agenda.on('complete', (job) => {
-    logger.debug(`Job ${job.attrs.name} finished`);
-  });
-  agenda.on('success', (job) => {
-    logger.debug(`Job ${job.attrs.name} success`);
-  });
-  agenda.on('fail', (err, job) => {
-    logger.debug(`@@@@@@@@@@@@@@@@@@@Job ${job.attrs.name} failed with error: ${err.message}`);
-  });
+  // agenda.on('start', (job) => {
+  //   logger.debug(`Job ${job.attrs.name} starting`);
+  // });
+  // agenda.on('complete', (job) => {
+  //   logger.debug(`Job ${job.attrs.name} finished`);
+  // });
+  // agenda.on('success', (job) => {
+  //   logger.debug(`Job ${job.attrs.name} success`);
+  // });
+  // agenda.on('fail', (err, job) => {
+  //   logger.debug(`@@@@@@@@@@@@@@@@@@@Job ${job.attrs.name} failed with error: ${err.message}`);
+  // });
 
-  agenda.on('error', (err, job) => {
-    logger.debug(`@@@@@@@@@@@@@@@@@@@Job ${job.attrs.name} failed with error: ${err.message}`);
-  });
+  // agenda.on('error', (err, job) => {
+  //   logger.debug(`@@@@@@@@@@@@@@@@@@@Job ${job.attrs.name} failed with error: ${err.message}`);
+  // });
 }
 
 agenda.defaultLockLifetime(2000);
