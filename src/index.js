dc22a5cf95632d2cb09068cebd776f5b0580e2e9
ryann254 <keddelyronjoz@gmail.com>
2022-11-23 15:35:43 +0300

    feat: change production mongodb url

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-console */
 const mongoose = require('mongoose');
 const app = require('./app');
 const config = require('./config/config');
 const logger = require('./config/logger');
 
 let server;
+console.log(config.mongoose.url);
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   logger.info('Connected to MongoDB');
   server = app.listen(config.port, () => {
